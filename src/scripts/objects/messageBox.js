b9c2784494b78ac1955bfbf223f6ffbbf59a7f42
Zack <cstony0917@gmail.com>
2020-01-01 22:13:56 +0800

    change messagebox direction

--- src/scripts/objects/messageBox.js
+++ src/scripts/objects/messageBox.js
@@ -2,9 +2,9 @@ export default class FpsText extends Phaser.GameObjects.Text {
   texts = []
   constructor (scene) {
     const { width, height } = scene.cameras.main
-    super(scene, width - 10, height - 118, '', { color: 'black', fontSize: '20px' })
+    super(scene, 5, height - 118, '', { color: 'black', fontSize: '20px' })
     scene.add.existing(this)
-    this.setOrigin(1, 0)
+    // this.setOrigin(1, 0)
   }
 
   add (text) {
