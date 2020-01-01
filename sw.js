aa5683ccdbed89e7cdd0c61dd4f067f3a700e695
Zack <cstony0917@gmail.com>
2020-01-01 19:29:06 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.ef3b16703eb93ae727bd31461e04035a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.ba84ffe60ca43ec36b533ab89a0a9ef2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
