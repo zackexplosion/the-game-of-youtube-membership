7bb6c1ce7f42444ad1bf9a8c0e4fe9c82fa23f7c
Zack <cstony0917@gmail.com>
2020-01-01 18:22:54 +0800

    Updates

---
+++ precache-manifest.c2f32a97cc9de751cc22f103a78c993e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf71813395b6bc45abcf65c986b330ad",
+    "url": "CNAME"
+  },
+  {
+    "revision": "895ceb8ed81797129ff6372e8e413d1c",
+    "url": "assets/logo.png"
+  },
+  {
+    "revision": "1e81f65b28b4fe48bc0ef0c35fc6eea5",
+    "url": "assets/memberlist.json"
+  },
+  {
+    "revision": "fb050e4db28c2060277b59dae58c0857",
+    "url": "assets/music-en.mp3"
+  },
+  {
+    "revision": "1919367be5cd1f61feb654175176a28b",
+    "url": "assets/music.mp3"
+  },
+  {
+    "revision": "d395e197e42cb28d79d5ec6acd8ab805",
+    "url": "assets/player.png"
+  },
+  {
+    "revision": "b8b26599f0250d18c5874616f88033ab",
+    "url": "assets/zack2.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "e838edcc38be16daa763eaf412aafee4",
+    "url": "index.html"
+  },
+  {
+    "revision": "8348f9a4bbeddeb60ee5",
+    "url": "main.8d008479752b13095989.bundle.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "bcdd4ff3544ce5652e47",
+    "url": "vendors.908ceb97a3a59d7351a3.bundle.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.c2f32a97cc9de751cc22f103a78c993e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
