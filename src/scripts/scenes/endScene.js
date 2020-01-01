3e87187c169cff15c972363320be4e1bc35b4040
Zack <cstony0917@gmail.com>
2020-01-01 23:56:11 +0800

    add auto restart

--- src/scripts/scenes/endScene.js
+++ src/scripts/scenes/endScene.js
@@ -1,4 +1,6 @@
 export default class EndScene extends Phaser.Scene {
+  timedEvent
+  restartButton
   constructor () {
     super({ key: 'EndScene' })
   }
@@ -8,13 +10,23 @@ export default class EndScene extends Phaser.Scene {
     const { centerX, centerY } = this.cameras.main
     var text = this.add.text(centerX, centerY, '謝謝大家的支持:D', { color: 'black', fontSize: '40px' })
 
-    var restart = this.add.text(centerX, this.cameras.main.centerY + 100, '再看一次', { color: 'black', fontSize: '20px' })
+    this.restartButton = this.add.text(centerX, this.cameras.main.centerY + 100, '再看一次', { color: 'black', fontSize: '20px' })
       .setInteractive()
       .on('pointerup', e => {
         window.location.reload()
       })
 
     text.setOrigin(0.5)
-    restart.setOrigin(0.5)
+    this.restartButton.setOrigin(0.5)
+
+    this.timedEvent = this.time.delayedCall(20000, function () {
+      window.location.reload()
+    }, [], this)
+  }
+
+  update () {
+    var a = this.timedEvent
+    const l = ((a.delay - a.elapsed) / 1000).toFixed(0)
+    this.restartButton.setText('再看一次: ' + l)
   }
 }
