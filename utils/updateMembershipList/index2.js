620174384739f66dbf2ad49f1826337396abaed9
Zack <cstony0917@gmail.com>
2020-04-16 03:46:34 +0800

    add nvmrc

--- utils/updateMembershipList/index2.js
+++ utils/updateMembershipList/index2.js
@@ -1,99 +1,125 @@
-// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+var fs = require('fs')
+var readline = require('readline')
+var { google } = require('googleapis')
+var OAuth2 = google.auth.OAuth2
 
-'use strict';
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/'
+var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json'
 
-const {OAuth2Client} = require('google-auth-library');
-const http = require('http');
-const url = require('url');
-const opn = require('opn');
-const destroyer = require('server-destroy');
-
-// Download your OAuth2 configuration from the Google
-const keys = require('./credentials2.json');
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets (err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err)
+    return
+  }
+  // Authorize a client with the loaded credentials, then call the YouTube API.
+  authorize(JSON.parse(content), getChannel)
+})
 
 /**
- * Start by acquiring a pre-authenticated oAuth2 client.
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
  */
-async function main() {
-  const oAuth2Client = await getAuthenticatedClient();
-  // Make a simple request to the People API using our pre-authenticated client. The `request()` method
-  // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
-  const url = 'https://www.googleapis.com/youtube/v3/sponsors?part=snippet&filter=all';
-  const res = await oAuth2Client.request({url});
-  console.log(res.data);
+function authorize (credentials, callback) {
+  var clientSecret = credentials.installed.client_secret
+  var clientId = credentials.installed.client_id
+  var redirectUrl = credentials.installed.redirect_uris[0]
+  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl)
 
-  // After acquiring an access_token, you may want to check on the audience, expiration,
-  // or original scopes requested.  You can do that with the `getTokenInfo` method.
-  const tokenInfo = await oAuth2Client.getTokenInfo(
-    oAuth2Client.credentials.access_token
-  );
-  console.log(tokenInfo);
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function (err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback)
+    } else {
+      oauth2Client.credentials = JSON.parse(token)
+      callback(oauth2Client)
+    }
+  })
 }
-const SCOPES = [
-  'https://www.googleapis.com/auth/youtube.readonly'
-]
+
 /**
- * Create a new OAuth2Client, and go through the OAuth2 content
- * workflow.  Return the full client to the callback.
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
  */
-function getAuthenticatedClient() {
-  return new Promise((resolve, reject) => {
-    // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
-    // which should be downloaded from the Google Developers Console.
-    const oAuth2Client = new OAuth2Client(
-      keys.web.client_id,
-      keys.web.client_secret,
-      keys.web.redirect_uris[0]
-    );
-
-    // Generate the url that will be used for the consent dialog.
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
-
-    // Open an http server to accept the oauth callback. In this simple example, the
-    // only request to our webserver is to /oauth2callback?code=<code>
-    const server = http
-      .createServer(async (req, res) => {
-        try {
-          if (req.url.indexOf('/oauth2callback') > -1) {
-            // acquire the code from the querystring, and close the web server.
-            const qs = new url.URL(req.url, 'http://localhost:3000')
-              .searchParams;
-            const code = qs.get('code');
-            console.log(`Code is ${code}`);
-            res.end('Authentication successful! Please return to the console.');
-            server.destroy();
-
-            // Now that we have the code, use that to acquire tokens.
-            const r = await oAuth2Client.getToken(code);
-            // Make sure to set the credentials on the OAuth2 client.
-            oAuth2Client.setCredentials(r.tokens);
-            console.info('Tokens acquired.');
-            resolve(oAuth2Client);
-          }
-        } catch (e) {
-          reject(e);
-        }
-      })
-      .listen(3000, () => {
-        // open the browser to the authorize url to start the workflow
-        opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
-      });
-    destroyer(server);
-  });
+function getNewToken (oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  })
+  console.log('Authorize this app by visiting this url: ', authUrl)
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.question('Enter the code from that page here: ', function (code) {
+    rl.close()
+    oauth2Client.getToken(code, function (err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err)
+        return
+      }
+      oauth2Client.credentials = token
+      storeToken(token)
+      callback(oauth2Client)
+    })
+  })
 }
 
-main().catch(console.error);
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken (token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR)
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) throw err
+    console.log('Token stored to ' + TOKEN_PATH)
+  })
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getChannel (auth) {
+  var service = google.youtube('v3')
+  service.channels.list({
+    auth: auth,
+    part: 'snippet,contentDetails,statistics',
+    forUsername: 'GoogleDevelopers'
+  }, function (err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err)
+      return
+    }
+    var channels = response.data.items
+    if (channels.length === 0) {
+      console.log('No channel found.')
+    } else {
+      console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
+                  'it has %s views.',
+      channels[0].id,
+      channels[0].snippet.title,
+      channels[0].statistics.viewCount)
+    }
+  })
+}
