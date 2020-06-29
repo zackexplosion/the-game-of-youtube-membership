e4d9f0420bcb81de58bc2a74b11bc437d2e6998b
Zack <cstony0917@gmail.com>
2020-06-30 00:47:34 +0800

    remove pause overlay, replace by dom overlay

--- src/gamedata/gameconfig.ts
+++ src/gamedata/gameconfig.ts
@@ -3,7 +3,6 @@ import MainScene from '@/scenes/mainScene'
 import LoadingScene from '@/scenes/LoadingScene'
 import EndScene from '@/scenes/endScene'
 import TestScene from '@/scenes/TestScene'
-import PauseScene from '@/scenes-ui/PauseScene'
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -21,7 +20,6 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
   },
   scene: [
     LoadingScene,
-    PauseScene,
     MenuScene,
     MainScene,
     EndScene,
--- src/helpers/OrientationChecker.ts
+++ src/helpers/OrientationChecker.ts
@@ -1,7 +1,5 @@
-import PauseScene from '@/scenes-ui/PauseScene'
-
 type OrientationchangeCallbackType = (isValid: boolean) => void;
-const PAUSE_SCENE_KEY: string = 'PauseScene'
+const PAUSE_OVERLAY_SHOW_CLASS: string = 'show'
 export default class OrientationChecker {
   gameScene: Phaser.Scene
   onOrientationchangeCallback: OrientationchangeCallbackType
@@ -9,13 +7,14 @@ export default class OrientationChecker {
 
   constructor(gameScene: Phaser.Scene) {
     this.gameScene = gameScene
-
     this.currentSceneKey = gameScene.scene.key
 
+    const pauseOverlay = document.querySelector('#invalid-orientation-pause-overlay')
+
     // check devise orientation on not desktop device
-    if (this.isMobilePortrait()) {
+    if (this.isMobilePortrait() && pauseOverlay) {
       gameScene.scene.pause(this.currentSceneKey)
-      gameScene.scene.launch(PAUSE_SCENE_KEY)
+      pauseOverlay.className = PAUSE_OVERLAY_SHOW_CLASS
     }
 
     gameScene.game.scale.on('orientationchange', (orientation: Phaser.Scale.Orientation) => {
@@ -23,17 +22,13 @@ export default class OrientationChecker {
       if (orientation === Phaser.Scale.Orientation.LANDSCAPE) {
         isValid = true
         gameScene.scene.resume(this.currentSceneKey)
-        gameScene.scene.bringToTop(this.currentSceneKey)
       } else {
-        // launch the pause secene when orientation is not valid
-        gameScene.scene.launch(PAUSE_SCENE_KEY)
-        gameScene.scene.bringToTop(PAUSE_SCENE_KEY)
         gameScene.scene.pause(this.currentSceneKey)
       }
 
-      // gameScene.scene.switch(PAUSE_SCENE_KEY)
-      // set the visiable for the pause scene
-      gameScene.scene.setVisible(!isValid, PAUSE_SCENE_KEY)
+      if (pauseOverlay) {
+        pauseOverlay.className = isValid ? '' : PAUSE_OVERLAY_SHOW_CLASS
+      }
 
 
       if (typeof this.onOrientationchangeCallback === 'function') {
