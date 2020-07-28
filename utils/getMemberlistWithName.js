90e6c298c3391a2f4397351dfa823c04106cea17
Zack <cstony0917@gmail.com>
2020-07-28 19:49:06 +0800

    --amend
    update gamedata

---
+++ utils/getMemberlistWithName.js
@@ -0,0 +1,24 @@
+var m = require('../src/gamedata/memberlist.json')
+var r = m.map( _ => _[1])
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+shuffle(r)
+console.log(r.join(' '))
